1a01b905efe42cb8d95c1607996c1983b1548bda
LoveKach <cryptonitik@gmail.com>
2023-08-20 09:11:01 +0300

    rss

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function toReadable (number) {
-  
+  const numbersName = ['zero', 'one','two','three','four','five','six','seven','eight','nine'];
+  const numbers = 
+  const result = ;
+  return result;
+
 }
+
